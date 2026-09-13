9c4d23a0d4bfe38cc3bfa2c9169963a6d086d440
shashwat8480 <shashwat180805@gmail.com>
2026-09-14 01:49:16 +0530

    Add: reorganize-string - submission-1

---
+++ Data Structures & Algorithms/reorganize-string/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s)
+        maxHeap = [[-cnt, char] for char,cnt in count.items()]
+        heapq.heapify(maxHeap)
+
+        res = [] 
+        q = deque()
+
+        while maxHeap: 
+            count, char = heapq.heappop(maxHeap)
+            count += 1
+            res.append(char)
+
+            q.append([count,char])
+
+            if len(q) >= 2: 
+                oldcount,oldchar = q.popleft()
+                if oldcount != 0: 
+                    heapq.heappush(maxHeap,[oldcount,oldchar])
+        
+        if len(res) != len(s): 
+            return ""
+        
+        return "".join(res)
+
+
+            
